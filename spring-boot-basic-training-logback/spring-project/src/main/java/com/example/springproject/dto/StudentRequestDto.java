e43b921bfa48aabc3748f4fac69c0eb2349bbbf9
KavingaW <kavinga@hsenidlanka.com>
2023-04-11 08:32:37 +0530

    added spring boot basic training logback directory

---
+++ spring-boot-basic-training-logback/spring-project/src/main/java/com/example/springproject/dto/StudentRequestDto.java
@@ -0,0 +1,40 @@
+package com.example.springproject.dto;
+
+public class StudentRequestDto {
+    private int id;
+    private String name;
+    private String dob;
+    private float average;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public float getAverage() {
+        return average;
+    }
+
+    public void setAverage(float average) {
+        this.average = average;
+    }
+}
